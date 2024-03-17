2d8416614e48622736fc5b1cbe971d6993b68604
Luan Simões <luaan.carlos@hotmail.com>
2024-03-16 23:56:31 -0300

    arrumando eslint

--- src/content/home/banners/loading.tsx
+++ src/content/home/banners/loading.tsx
@@ -3,15 +3,15 @@ export function Loading() {
     <div className="flex animate-pulse justify-center">
       <div className="relative flex w-full max-w-[1365px] items-center justify-end">
         <div className="absolute left-0 z-[1] mb-24 ml-auto flex w-full flex-col items-center gap-8 lg:w-auto lg:items-start">
-          <div className="bg-gray-450 h-[40px] w-[400px]" />
-          <div className="bg-gray-450 h-[30px] w-[200px]" />
+          <div className="h-[40px] w-[400px] bg-gray-450" />
+          <div className="h-[30px] w-[200px] bg-gray-450" />
 
           <div className="flex items-center gap-24">
-            <div className="bg-gray-450 h-[24px] w-[110px] rounded-sm" />
+            <div className="h-[24px] w-[110px] rounded-sm bg-gray-450" />
 
             <div className="hidden gap-5 lg:flex">
-              <div className="bg-gray-450 h-[24px] w-[24px] rounded-sm" />
-              <div className="bg-gray-450 h-[24px] w-[24px] rounded-sm" />
+              <div className="h-[24px] w-[24px] rounded-sm bg-gray-450" />
+              <div className="h-[24px] w-[24px] rounded-sm bg-gray-450" />
             </div>
           </div>
         </div>
